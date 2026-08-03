ae5129d354247b9351431571fea29318e61cd3b5
Muhammad Zeeshan <muhammadzeeshan@nodeus.tech>
2026-08-03 21:23:53 +0500

    Update Contact Form

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -21,9 +21,30 @@ const router = express.Router();
    EMAIL TRANSPORTER
 ========================================= */
 
+// const transporter = nodemailer.createTransport({
+
+//   service: "gmail",
+
+//   auth: {
+
+//     user: process.env.EMAIL_USER,
+
+//     pass: process.env.EMAIL_PASS,
+
+//   },
+
+// });
+
+
 const transporter = nodemailer.createTransport({
 
-  service: "gmail",
+  host: process.env.SMTP_HOST,
+
+  port: Number(
+    process.env.SMTP_PORT
+  ),
+
+  secure: true,
 
   auth: {
 
