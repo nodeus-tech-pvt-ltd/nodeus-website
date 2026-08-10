66367abdfc75070cf27d48d019fde5a4d65fabc7
Muhammad Zeeshan <muhammadzeeshan@nodeus.tech>
2026-08-10 18:29:40 +0500

    Update Small Changes

--- src/data/team/teamData.js
+++ src/data/team/teamData.js
@@ -12,7 +12,7 @@ import muhammadGulsher from "../../assets/team/Muhammad Gulsher.png";
 
 import hamidSaeedKhan from "../../assets/team/Hamid Saeed Khan.png";
 
-import uraidUlHassan from "../../assets/team/Uraid ul Hassan.jpg";
+import uraidUlHassan from "../../assets/team/Uraid ul Hassan.png";
 
 import abdullahHafeez from "../../assets/team/Abdullah Hafeez.png";
 
